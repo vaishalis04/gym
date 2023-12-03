80b5c3b6e56c83b70b368670c19f422ce02c87a3
vaishali singh <vaishalisingh00q@gmail.com>
2023-12-03 19:58:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -30, ease: "power2.out" });
+
+gsap.from(".nav-list li", { opacity: 0, duration: 1, delay: 1, y: -30, stagger: 0.2, ease: "power2.out" });
+gsap.from(".hero h1", { opacity: 0, duration: 1, delay: 0.5, y: -30, ease: "power2.out" });
+gsap.from(".hero p", { opacity: 0, duration: 1, delay: 1, y: -30, ease: "power2.out" });
+gsap.from(".hero .cta-button", { opacity: 0, duration: 1, delay: 1.5, y: -30, ease: "power2.out" });
+gsap.from(".page-header h1", { opacity: 0, duration: 1, delay: 1.5, y: -30, ease: "power2.out" });
+gsap.from(".page-header p", { opacity: 0, duration: 1, delay: 2, y: -30, ease: "power2.out" });
+gsap.from(".class-card", { opacity: 0, duration: 1, delay: 3.5, y: -30, stagger: 0.2, ease: "power2.out" });
+// gsap.from(".page-header h1", { opacity: 0, duration: 1, delay: 0.5, y: -30, ease: "power2.out" });
+// gsap.from(".page-header p", { opacity: 0, duration: 1, delay: 1, y: -30, ease: "power2.out" });
+gsap.from(".schedule-table", { opacity: 0, duration: 1, delay: 5.5, y: -30, ease: "power2.out" });
+// gsap.from(".page-header h1", { opacity: 0, duration: 1, delay: 0.5, y: -30, ease: "power2.out" });
+// gsap.from(".page-header p", { opacity: 0, duration: 1, delay: 1, y: -30, ease: "power2.out" });
+gsap.from(".pricing-card", { opacity: 0, duration: 1, delay: 7.5, y: -30, stagger: 0.2, ease: "power2.out" });
+// gsap.from(".page-header h1", { opacity: 0, duration: 1, delay: 0.5, y: -30, ease: "power2.out" });
+// gsap.from(".page-header p", { opacity: 0, duration: 1, delay: 1, y: -30, ease: "power2.out" });
+gsap.from(".contact-form", { opacity: 0, duration: 1, delay: 9.5, y: -30, ease: "power2.out" });
